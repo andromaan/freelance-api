2df72bc2730ad7d1055c89be7bfa5161a3709416
Andrii <andrii.lavreniuk@oa.edu.ua>
2026-05-20 21:34:50 +0300

    feat: project creation now with categories

---
+++ src/BLL/CommandsQueries/Projects/Handlers/CreateProjectHandler.cs
@@ -0,0 +1,28 @@
+using BLL.Common.Handlers;
+using BLL.Common.Interfaces.Repositories.Categories;
+using BLL.Services;
+using BLL.ViewModels.Project;
+using Domain.Models.Projects;
+
+namespace BLL.CommandsQueries.Projects.Handlers;
+
+public class CreateProjectHandler(ICategoryQueries categoryQueries) : ICreateHandler<Project, CreateProjectVM>
+{
+    public async Task<ServiceResponse?> HandleAsync(Project entity, CreateProjectVM createModel, CancellationToken cancellationToken)
+    {
+        var categoriesIds = createModel.CategoryIds.Distinct();
+        
+        foreach (var categId in categoriesIds)
+        {
+            var existingCategory = await categoryQueries.GetByIdAsync(categId, cancellationToken);
+            if (existingCategory == null)
+            {
+                return ServiceResponse.NotFound($"Category with id {categId} not found");
+            }
+
+            entity.Categories.Add(existingCategory);
+        }
+        
+        return ServiceResponse.Ok();
+    }
+}
--- src/BLL/ViewModels/Project/CreateProjectVM.cs
+++ src/BLL/ViewModels/Project/CreateProjectVM.cs
@@ -1,11 +1,11 @@
-using BLL.Common.Interfaces;
-
 namespace BLL.ViewModels.Project;
 
-public class CreateProjectVM : ISkipMapper
+public class CreateProjectVM
 {
     public required string Title { get; set; }
     public string? Description { get; set; }
     public decimal Budget { get; set; }
     public DateTime Deadline { get; set; }
+
+    public List<int> CategoryIds { get; set; } = new();
 }
